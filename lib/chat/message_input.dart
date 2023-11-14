84727a724ad20eb09d36b37bd1d81e7c6e559a57
Kushtrim Mulliqi <mulliqi13@live.com>
2023-11-14 01:49:44 +0100

    re designed message input

--- lib/chat/message_input.dart
+++ lib/chat/message_input.dart
@@ -15,23 +15,49 @@ class MessageInputBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: TextField(
-            controller: messageController,
-            decoration: const InputDecoration(
-              hintText: "Enter your message...",
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.blueGrey,
+            Color.fromARGB(255, 71, 124, 150),
+          ], // Gradient colors
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: messageController,
+              decoration: InputDecoration(
+                  hintText: "Enter your message...",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white12),
             ),
           ),
-        ),
-        IconButton(
-          icon: const Icon(Icons.send),
-          onPressed: () {
-            sendMessage(roomId, userId, messageController, context);
-          },
-        ),
-      ],
+          IconButton(
+            icon: const Icon(Icons.attach_file),
+            onPressed: () {
+              // Implement file attachment functionality
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () {
+              if (messageController.text.trim().isNotEmpty) {
+                sendMessage(roomId, userId, messageController, context);
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
