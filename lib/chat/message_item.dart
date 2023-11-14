a305c0b9f609dc0f2966a18512d634e3e39c5f73
Kushtrim Mulliqi <mulliqi13@live.com>
2023-11-14 01:49:56 +0100

    re designed message item

--- lib/chat/message_item.dart
+++ lib/chat/message_item.dart
@@ -1,5 +1,4 @@
 import 'package:chat/chat/chat_utilities/chat_utils.dart';
-import 'package:chat/profile/chat_room_profile.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -26,105 +25,101 @@ class MessageItem extends StatelessWidget {
     final isCurrentUser = userId == messageSender;
     final timestamp = message['timestamp'] as Timestamp?;
     final formattedDate = timestamp != null
-        ? DateFormat('hh:mm ').format(timestamp.toDate())
+        ? DateFormat('hh:mm').format(timestamp.toDate())
         : 'Unknown time';
+    final profileImage = message['profileImage'];
 
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Row(
-        mainAxisAlignment:
-            isCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      child: Stack(
         children: [
-          buildProfileWidget(context, messageSender ?? ''),
-          const SizedBox(width: 10.0),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildMessageSenderNameandDeleteButton(
-                    messageSenderName ?? '', messageSender ?? ''),
-                buildMessageTextandTime(messageText ?? '', formattedDate),
-              ],
-            ),
+          Row(
+            mainAxisAlignment:
+                isCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              if (!isCurrentUser)
+                CircleAvatar(
+                  backgroundImage:
+                      profileImage != null ? NetworkImage(profileImage) : null,
+                  child: profileImage == null
+                      ? const Icon(Icons.person, size: 30)
+                      : null,
+                ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.blueGrey,
+                        Color.fromARGB(255, 71, 124, 150),
+                      ],
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        messageSenderName ?? '',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        messageText ?? '',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            formattedDate,
+                            style: const TextStyle(
+                              color: Colors.white70,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              if (isAdmin || isModerator)
+                IconButton(
+                  icon: const Icon(Icons.more_vert),
+                  onPressed: () {
+                    invokeOptionsDialog(messageSender, userId, context);
+                  },
+                ),
+            ],
           ),
-          if ((isAdmin || isModerator))
-            iconButton(messageSender ?? '', userId, context),
+          if (isAdmin || isModerator) buildDeleteButton(context),
         ],
       ),
     );
   }
 
-  Widget buildMessageSenderNameandDeleteButton(
-      String messageSenderName, String messageSender) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          messageSenderName,
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        if ((isAdmin || isModerator))
-          IconButton(
-            icon: const Icon(
-              Icons.remove_circle_outline_outlined,
-              color: Colors.red,
-            ),
-            onPressed: () {
-              deleteMessage(
-                messageSender,
-                message.id,
-              );
-            },
-          ),
-      ],
-    );
-  }
-
-  Widget buildMessageTextandTime(String messageText, String formattedDate) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Flexible(
-          child: Text(
-            messageText,
-            style: const TextStyle(
-              color: Colors.black,
-            ),
-          ),
-        ),
-        const SizedBox(width: 10.0),
-        Text(
-          formattedDate,
-          style: const TextStyle(
-            color: Colors.black,
-            fontSize: 10.0,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget buildProfileWidget(BuildContext context, String messageSender) {
-    final profileImage = message['profileImage'];
-    final isCurrentUser = userId == messageSender;
-
-    return InkWell(
-      onTap: () {
-        if (!isCurrentUser) {
-          showUserDetails(messageSender, context);
-        }
-      },
-      child: Align(
-        alignment: isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
-        child: CircleAvatar(
-          backgroundImage:
-              profileImage != null ? NetworkImage(profileImage) : null,
-          child:
-              profileImage == null ? const Icon(Icons.person, size: 30) : null,
-        ),
+  Widget buildDeleteButton(BuildContext context) {
+    return Positioned(
+      top: -10,
+      right: 40,
+      child: IconButton(
+        icon: const Icon(Icons.close, color: Colors.white),
+        onPressed: () {
+          deleteMessage(message['senderId'], message.id);
+        },
       ),
     );
   }
