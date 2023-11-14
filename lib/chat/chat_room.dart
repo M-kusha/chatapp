821a76a3b0f9bc26498adbfde34b7ed42d370283
Kushtrim Mulliqi <mulliqi13@live.com>
2023-11-14 01:49:23 +0100

    re designed chat room

--- lib/chat/chat_room.dart
+++ lib/chat/chat_room.dart
@@ -2,22 +2,28 @@ import 'package:chat/chat/chat_utilities/check_ban_kick_alert.dart';
 import 'package:chat/chat/message_input.dart';
 import 'package:chat/chat/message_list.dart';
 import 'package:chat/chat/chat_utilities/chat_utils.dart';
-import 'package:chat/utilities/side_navigation.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatRoom extends StatefulWidget {
   final String roomId;
   final String userId;
+  final String roomName;
 
-  const ChatRoom({Key? key, required this.roomId, required this.userId})
-      : super(key: key);
+  const ChatRoom({
+    Key? key,
+    required this.roomId,
+    required this.userId,
+    required this.roomName,
+    required bool isLocked,
+  }) : super(key: key);
 
   @override
   ChatRoomState createState() => ChatRoomState();
 }
 
 class ChatRoomState extends State<ChatRoom> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final TextEditingController _messageController = TextEditingController();
   bool isAdmin = false;
   bool isModerator = false;
@@ -41,19 +47,48 @@ class ChatRoomState extends State<ChatRoom> {
     kickStatusStream = checkUserKicked(widget.userId).asStream();
   }
 
+  void userLeftChatRoom() {
+    _messageController.clear();
+    decreaseOnlineUdesersCount(widget.roomId);
+    Navigator.pop(context);
+  }
+
+  void decreaseOnlineUdesersCount(String roomId) {
+    final roomRef = _firestore.collection('chat_rooms').doc(roomId);
+    roomRef.update({
+      'online_users_count': FieldValue.increment(-1),
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: widget.roomId == 'general'
-            ? const Text('General Chat')
-            : const Text('Chat Room'),
+        title:
+            Text(widget.roomName, style: const TextStyle(color: Colors.white)),
         centerTitle: true,
-      ),
-      drawer: SideNavigationBar(
-        handleLogout: () async {
-          await FirebaseAuth.instance.signOut();
-        },
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.blueGrey,
+                Color.fromARGB(255, 71, 124, 150),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout, color: Colors.white),
+            onPressed: () async {
+              userLeftChatRoom();
+            },
+          ),
+        ],
       ),
       body: StreamBuilder<KickStatus>(
         stream: kickStatusStream,
@@ -69,39 +104,36 @@ class ChatRoomState extends State<ChatRoom> {
             if (kickStatus.isKicked) {
               final remainingTime =
                   kickStatus.kickExpiration.difference(DateTime.now());
-
               _showKickAlert(remainingTime);
-
-              // Immediately navigate out of the chat room
-              _navigateOutOfChatRoom();
-
-              // Return an empty widget
+              userLeftChatRoom();
               return const SizedBox();
             }
 
-            // Render the chat room if not kicked
-            return Column(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: MessageList(
-                      roomId: widget.roomId,
-                      userId: widget.userId,
-                      isAdmin: isAdmin,
-                      isModerator: isModerator,
+            return Container(
+              color: Colors.grey[300],
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: MessageList(
+                        roomId: widget.roomId,
+                        userId: widget.userId,
+                        isAdmin: isAdmin,
+                        isModerator: isModerator,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: MessageInputBar(
-                    messageController: _messageController,
-                    roomId: widget.roomId,
-                    userId: widget.userId,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MessageInputBar(
+                      messageController: _messageController,
+                      roomId: widget.roomId,
+                      userId: widget.userId,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             );
           }
         },
@@ -118,14 +150,5 @@ class ChatRoomState extends State<ChatRoom> {
         );
       },
     );
-  }
-
-  void _navigateOutOfChatRoom() {
-    setState(() {
-      // Clear the message input controller if needed
-      _messageController.clear();
-    });
-
-    Navigator.pop(context);
   }
 }
